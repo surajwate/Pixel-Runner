da34e6c4179dd104fef88b4ac17d67ed0362a273
Suraj Wate <suraj.wate@gmail.com>
2024-11-07 16:58:29 +0000

    Refactor snail movement and add player character to game display

--- game.py
+++ game.py
@@ -9,25 +9,29 @@ test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
 sky_surface = pygame.image.load("graphics/Sky.png").convert()
 ground_surface = pygame.image.load("graphics/ground.png").convert()
-text_surface = test_font.render("My Game", False, 'black')
+text_surface = test_font.render("My Game", False, "black")
 
 snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
-snail_x_pos = 600
+snail_rectangle = snail_surface.get_rect(midbottom=(600, 300))
+
+player_surface = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
+player_reactange = player_surface.get_rect(midbottom=(80, 300))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    
+
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
-    snail_x_pos -= 4
-    if snail_x_pos <= -100:
-        snail_x_pos = 800
-    screen.blit(snail_surface, (snail_x_pos, 250))
 
+    if snail_rectangle.right <= 0:
+        snail_rectangle.left = 800
+    snail_rectangle.left -= 4
+    screen.blit(snail_surface, snail_rectangle)
+    screen.blit(player_surface, player_reactange)
 
     pygame.display.update()
     clock.tick(60)
