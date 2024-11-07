1fb3fd60b4e6e4c06223e18d069cfcaa61885655
Suraj Wate <suraj.wate@gmail.com>
2024-11-07 08:19:57 +0000

    Add title text to game display

--- game.py
+++ game.py
@@ -5,9 +5,11 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner Game")
 clock = pygame.time.Clock()
+test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
 sky_surface = pygame.image.load("graphics/Sky.png")
 ground_surface = pygame.image.load("graphics/ground.png")
+text_surface = test_font.render("My Game", False, 'green')
 
 while True:
     for event in pygame.event.get():
@@ -17,5 +19,8 @@ while True:
     
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
+
+
     pygame.display.update()
     clock.tick(60)
