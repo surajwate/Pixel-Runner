3e2ccd53e42c460d8aa2339ffc989d8c1928a835
Suraj Wate <suraj.wate@gmail.com>
2024-11-10 00:40:02 +0000

    Add score display and update rendering logic in game

--- game.py
+++ game.py
@@ -9,7 +9,9 @@ test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
 sky_surface = pygame.image.load("graphics/Sky.png").convert()
 ground_surface = pygame.image.load("graphics/ground.png").convert()
-text_surface = test_font.render("My Game", False, "black")
+
+score_surface = test_font.render("My Game", False, "black")
+score_rect = score_surface.get_rect(center=(400, 50))
 
 snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
 snail_rectangle = snail_surface.get_rect(midbottom=(600, 300))
@@ -22,14 +24,14 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEMOTION:
-            # print(event.pos)
-            if player_reactange.collidepoint(event.pos):
-                print("Collision")
+        # if event.type == pygame.MOUSEMOTION:
+        #     # print(event.pos)
+        #     if player_reactange.collidepoint(event.pos):
+        #         print("Collision")
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (300, 50))
+    screen.blit(score_surface, score_rect)
 
     if snail_rectangle.right <= 0:
         snail_rectangle.left = 800
