7fa914761a335358ac0e94678cef385c2c3e66d5
Suraj Wate <suraj.wate@gmail.com>
2024-11-10 01:01:12 +0000

    Change score display color to dark gray and update border color to light blue

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 sky_surface = pygame.image.load("graphics/Sky.png").convert()
 ground_surface = pygame.image.load("graphics/ground.png").convert()
 
-score_surface = test_font.render("My Game", False, "black")
+score_surface = test_font.render("My Game", False, (64, 64, 64))
 score_rect = score_surface.get_rect(center=(400, 50))
 
 snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
@@ -31,8 +31,8 @@ while True:
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    pygame.draw.rect(screen, "pink", score_rect)
-    pygame.draw.rect(screen, "pink", score_rect, 10)
+    pygame.draw.rect(screen, "#c0e8ec", score_rect)
+    pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
     screen.blit(score_surface, score_rect)
 
     if snail_rectangle.right <= 0:
