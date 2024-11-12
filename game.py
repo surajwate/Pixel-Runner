f86fbe73c8f277909682be533dca64190d394589
Suraj Wate <suraj.wate@gmail.com>
2024-11-12 14:53:53 +0000

    Add score display functionality to the game

--- game.py
+++ game.py
@@ -1,18 +1,26 @@
 import pygame
 from sys import exit
 
+def display_score():
+    current_time = pygame.time.get_ticks() - start_time
+    current_time /= 1000
+    score_surface = test_font.render(f"Score: {int(current_time)}", False, (64, 64, 64))
+    score_rect = score_surface.get_rect(center=(400, 50))
+    screen.blit(score_surface, score_rect)
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner Game")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 game_active = True
+start_time = 0
 
 sky_surface = pygame.image.load("graphics/Sky.png").convert()
 ground_surface = pygame.image.load("graphics/ground.png").convert()
 
-score_surface = test_font.render("My Game", False, (64, 64, 64))
-score_rect = score_surface.get_rect(center=(400, 50))
+# score_surface = test_font.render("My Game", False, (64, 64, 64))
+# score_rect = score_surface.get_rect(center=(400, 50))
 
 snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
 snail_rectangle = snail_surface.get_rect(midbottom=(600, 300))
@@ -40,13 +48,16 @@ while True:
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
+                snail_rectangle.left = 800
+                start_time = pygame.time.get_ticks()
 
     if game_active:
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300))
-        pygame.draw.rect(screen, "#c0e8ec", score_rect)
-        pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
-        screen.blit(score_surface, score_rect)
+        # pygame.draw.rect(screen, "#c0e8ec", score_rect)
+        # pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
+        # screen.blit(score_surface, score_rect)
+        display_score()
 
         if snail_rectangle.right <= 0:
             snail_rectangle.left = 800
